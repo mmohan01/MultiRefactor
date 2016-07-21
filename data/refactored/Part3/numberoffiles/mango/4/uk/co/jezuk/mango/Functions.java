5c1c1488db7cfd38626e5c19126b453923eb787b
mmohan01 <mmohan03@qub.ac.uk>
2016-07-21 21:16:14 +0100

    Added files.

---
+++ data/refactored/Part3/numberoffiles/mango/4/uk/co/jezuk/mango/Functions.java
@@ -0,0 +1,57 @@
+package uk.co.jezuk.mango;
+
+import uk.co.jezuk.mango.functions.*;
+
+/**
+ * <strong>The Mango Library Unary Functions</strong>
+ * <br/><br/>
+ * A <code>{@link Function}</code> describes a function which takes one argument
+ * - <code>fn(x)</code>. It returns some <code>T</code> and returns and 
+ * object of type <code>R</code>.  T and R may be the same time).  A 
+ * <code>Function</code> may return the same object or different objects 
+ * for each invocation, even given the same argument.  It can refer to 
+ * local state, perform disk reads or writes, or whatever.  
+ * @see Function
+ * @see Predicates
+ * @author Jez Higgins, jez@jezuk.co.uk
+ */
+public class Functions {
+  ////////////////////////////////////////////////////
+  // Function adaptors
+  /**
+   * Compose is a unary function adaptor.  If <code>f</code> and <code>g</code>
+   * are <code>Functions</code>, then <code>Compose</code> creates a new
+   * function <code>h</code>, where <code>h(x)</code> is equal to <code>f(g(x))</code>.
+   */
+    static public <T, T2, R> Function<T, R> Compose(final Function<T2, R> f,
+                          final Function<T, T2> g)
+     {
+        return new Function<T, R>(){
+            public R fn(final T x)
+             {
+                return f.fn(g.fn(x));
+            } // fn
+        };
+    } // Compose
+
+
+
+  ////////////////////////////////////////////////////
+  // Unary Functions
+  /**
+   * The identity function.  Takes a single argument, 
+   * and returns it unchanged.
+   */
+    static public <T> Function<T, T> Identity()
+     {
+        return (Function<T, T>)Identity.INSTANCE;
+    } // Identity
+
+  /**
+   * Returns the initial value, regardless of the argument
+   * passed to the function
+   */ public <T, R> Function<T, R> Constant(R value)
+     {
+        return (Function<T, R>)new Constant<R>(value);
+    } // Constant
+} // class Functions
---
+++ data/refactored/Part3/numberofmethods/jhotdraw-5.3/3/CH/ifa/draw/standard/FigureChangeAdapter.java
@@ -0,0 +1,54 @@
+/*
+ * @(#)FigureChangeAdapter.java
+ *
+ * Project:		JHotdraw - a GUI framework for technical drawings
+ *				http://www.jhotdraw.org
+ *				http://jhotdraw.sourceforge.net
+ * Copyright:	© by the original author(s) and all contributors
+ * License:		Lesser GNU Public License (LGPL)
+ *				http://www.opensource.org/licenses/lgpl-license.html
+ */
+
+package CH.ifa.draw.standard;
+
+import CH.ifa.draw.framework.*;
+import CH.ifa.draw.util.*;
+import CH.ifa.draw.util.*;
+import CH.ifa.draw.util.*;
+
+/**
+ * Empty implementation of FigureChangeListener.
+ *
+ * @version <$CURRENT_VERSION$>
+ */
+public abstract class FigureChangeAdapter implements FigureChangeListener {
+    public double LENGTH_FACTOR = 1.0;
+    public double FRICTION_FACTOR = 0.75;
+    public double TORQUE_STRENGTH = 0.25;
+
+    /**
+	 *  Sent when an area is invalid
+	 */
+    public void figureInvalidated(FigureChangeEvent e) {}
+
+    /**
+	 * Sent when a figure changed
+	 */
+    public void figureChanged(FigureChangeEvent e) {}
+
+    /**
+	 * Sent when a figure was removed
+	 */
+    public void figureRemoved(FigureChangeEvent e) {}
+
+    /**
+	 * Sent when requesting to remove a figure.
+	 */
+    public void figureRequestRemove(FigureChangeEvent e) {}
+
+    /**
+	 * Sent when an update should happen.
+	 *
+	 */
+    public void figureRequestUpdate(FigureChangeEvent e) {}
+}
