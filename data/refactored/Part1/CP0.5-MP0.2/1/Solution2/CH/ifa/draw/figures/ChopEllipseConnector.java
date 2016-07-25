c90a591e1f97e9485b93ca9b1a911ffe3d41f209
mmohan01 <mmohan03@qub.ac.uk>
2016-07-25 14:57:43 +0100

    Added files.

---
+++ data/refactored/Part1/CP0.5-MP0.2/1/Solution2/CH/ifa/draw/figures/ChopEllipseConnector.java
@@ -0,0 +1,64 @@
+/*
+ * @(#)ChopEllipseConnector.java
+ *
+ * Project:		JHotdraw - a GUI framework for technical drawings
+ *				http://www.jhotdraw.org
+ *				http://jhotdraw.sourceforge.net
+ * Copyright:	© by the original author(s) and all contributors
+ * License:		Lesser GNU Public License (LGPL)
+ *				http://www.opensource.org/licenses/lgpl-license.html
+ */
+
+package CH.ifa.draw.figures;
+
+import java.awt.*;
+import CH.ifa.draw.framework.*;
+import CH.ifa.draw.standard.*;
+import CH.ifa.draw.util.Geom;
+
+import java.awt.*;
+import CH.ifa.draw.framework.*;
+import CH.ifa.draw.standard.*;
+
+/**
+ * A ChopEllipseConnector locates a connection point by
+ * chopping the connection at the ellipse defined by the
+ * figure's display box.
+ *
+ * @version <$CURRENT_VERSION$>
+ */
+public class ChopEllipseConnector extends ChopBoxConnector {
+
+    /*
+	 * Serialization support.
+	 */
+    private static final long serialVersionUID = -3165091511154766610L;
+
+    public Point findEnd(ConnectionFigure connection) {
+        Figure endFigure = connection.getEndConnector().owner();
+        Rectangle r1 = connection.getStartConnector().displayBox();
+        Point r1c = null;
+
+        if (connection.pointCount() == 2) {
+            r1c = new Point(r1.x + r1.width / 2, r1.y + r1.height / 2);
+        }
+         else {
+            r1c = connection.pointAt(connection.pointCount() - 2);
+        }
+
+        return chop(endFigure, r1c);
+    }
+
+    public ChopEllipseConnector() {
+    }
+
+    public ChopEllipseConnector(Figure owner) {
+        super(owner);
+    }
+
+    protected Point chop(Figure target, Point from) {
+        Rectangle r = target.displayBox();
+        double angle = Geom.pointToAngle(r, from);
+        return Geom.ovalAngleToPoint(r, angle);
+    }
+}
---
+++ data/refactored/Part1/CP0.5-MP0.2/2/Solution7/CH/ifa/draw/contrib/ChopPolygonConnector.java
@@ -0,0 +1,61 @@
+/*
+ * @(#)ChopPolygonConnector.java
+ *
+ * Project:		JHotdraw - a GUI framework for technical drawings
+ *				http://www.jhotdraw.org
+ *				http://jhotdraw.sourceforge.net
+ * Copyright:	© by the original author(s) and all contributors
+ * License:		Lesser GNU Public License (LGPL)
+ *				http://www.opensource.org/licenses/lgpl-license.html
+ */
+
+package CH.ifa.draw.contrib;
+
+import java.awt.*;
+import CH.ifa.draw.framework.*;
+import CH.ifa.draw.standard.*;
+
+import java.awt.*;
+import CH.ifa.draw.framework.*;
+import CH.ifa.draw.standard.*;
+
+/**
+ * A ChopPolygonConnector locates a connection point by
+ * chopping the connection at the polygon boundary.
+ *
+ * @author Erich Gamma
+ * @version <$CURRENT_VERSION$>
+ */
+public class ChopPolygonConnector extends ChopBoxConnector {
+
+    /*
+	 * Serialization support.
+	 */
+    private static final long serialVersionUID = -156024908227796826L;
+
+    public ChopPolygonConnector() {
+    }
+
+    public ChopPolygonConnector(Figure owner) {
+        super(owner);
+    }
+
+    protected Point chop(Figure target, Point from) {
+        return ((PolygonFigure)target).chop(from);
+    }
+
+    public Point findEnd(ConnectionFigure connection) {
+        Figure endFigure = connection.getEndConnector().owner();
+        Rectangle r1 = connection.getStartConnector().displayBox();
+        Point r1c = null;
+
+        if (connection.pointCount() == 2) {
+            r1c = new Point(r1.x + r1.width / 2, r1.y + r1.height / 2);
+        }
+         else {
+            r1c = connection.pointAt(connection.pointCount() - 2);
+        }
+
+        return chop(endFigure, r1c);
+    }
+}
